e4bf098f5e9c7628834ce53f5b7f7e1d7d254764
Filip Åhfelt <filahf94@gmail.com>
2020-02-23 13:28:12 +0100

    crud implemented

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 //load env vars
 dotenv.config({ path: './config/config.env' });
@@ -10,6 +11,9 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+//Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -23,12 +27,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server and exit
   server.close(() => process.exit(1));
 });
